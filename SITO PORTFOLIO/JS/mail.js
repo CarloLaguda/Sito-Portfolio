3a02f59653fff6054ffa61e352a6e7d652eac35e
CarloLaguda <carlo.laguda07@gmail.com>
2024-04-28 15:05:21 +0200

    V1.0.3 code, graphic layout optmization

--- SITO PORTFOLIO/JS/mail.js
+++ SITO PORTFOLIO/JS/mail.js
@@ -1,11 +1,16 @@
 function inviaEmail(){
-    // Inizializza EmailJS con l'User ID
+    // Inizializza EmailJS con la public key
     emailjs.init('M2VR5CY4e3xmchZmS');
     // Recupera i valori dai campi del modulo
     const nome = document.getElementById('name');
     const contatto = document.getElementById('contact');
     const messaggio = document.getElementById('message');
     const btn = document.getElementById('button');
+    // Controlla se i campi sono vuoti
+    if (nome.value === '' || contatto.value === '' || messaggio.value === '') {
+        alert("Per favore, completa tutti i campi prima di inviare l'email.");
+        return; // Interrompe l'esecuzione della funzione se i campi sono vuoti
+    }
     // Crea un oggetto con i parametri per l'invio dell'email
     const params = {
         nome: nome.value,
@@ -13,7 +18,7 @@ function inviaEmail(){
         messaggio: messaggio.value
     };
     // Cambia il testo del pulsante durante l'invio
-    btn.textContent = 'Sending...';
+    btn.textContent = 'invio...';
     // Invia l'email tramite EmailJS
     emailjs.send("service_vf3n3sk", "template_sd6f2da", params)
     .then(function (response) {
@@ -27,7 +32,6 @@ function inviaEmail(){
     }, function (error) {
         // Mostra un messaggio di errore in caso di fallimento
         alert("Errore durante l'invio dell'email: " + error);
-
         // Resetta i campi del modulo in caso di errore
         btn.textContent = 'Invia';
         nome.value = '';
